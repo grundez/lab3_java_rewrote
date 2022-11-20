a839f2fd07206a0dc0171f729e97b000b8e871c6
Maksim <113585195+grundez@users.noreply.github.com>
2022-11-21 04:18:19 +0700

    Add files via upload

---
+++ internet_store/classes/helper.java
@@ -0,0 +1,40 @@
+package classes;
+import java.util.Scanner;
+
+public class helper {
+	private double helper_mark;
+	private String helper_name;
+	
+	//Конструктор класса без параметров.
+	public helper() {
+		helper_mark = 0;
+		helper_name = "нет имени..";
+	}
+
+	//Конструктор класса с одним параметром.
+	public helper(double mark) {
+		helper_mark = mark;
+		helper_name = "нет имени..";
+	}
+	
+	//Конструктор класса со всеми параметрами.
+	public helper(double mark, String name) {
+		helper_mark = mark;
+		helper_name = name;
+	}
+	
+	//Метод ввода.
+	public void helper_input() {
+		Scanner inpt = new Scanner(System.in);
+		System.out.print("Введите продавца: ");
+		helper_name = inpt.nextLine();
+		System.out.print("Введите оценку продавца: ");
+		helper_mark = inpt.nextDouble();
+		}
+
+	//Метод вывода.
+	public void helper_output() {
+		System.out.println("Имя помощника: " + helper_name);
+		System.out.println("Оценка помощника: " + helper_mark);
+	}
+}
---
+++ internet_store/classes/seller.java
@@ -0,0 +1,45 @@
+package classes;
+import java.util.Scanner;
+
+public class seller {
+	private int seller_sells;
+	private String seller_name;
+
+	//Конструктор класса без параметров.
+	public seller() {
+		seller_sells = 0;
+		seller_name = "Нет имени";
+	}
+
+	//Конструктор класса с одним параметром.
+	public seller(String name) {
+		seller_sells = 0;
+		seller_name = name;
+	}
+	
+	//Конструктор класса со всеми параметрами.
+	public seller(int sells, String name) {
+		seller_sells = sells;
+		seller_name = name;
+	}
+	
+	//Метод ввода.
+	public void seller_input() {
+		Scanner inpt = new Scanner(System.in);
+		System.out.print("Введите имя/название продавца: ");
+		seller_name = inpt.nextLine();
+		System.out.print("Введите количество продаж: ");
+		seller_sells = inpt.nextInt();
+	}
+
+	//Метод вывода.
+	public void seller_output() {
+		System.out.println("Имя/название продавца: " + seller_name);
+		System.out.println("Количество продаж: " + seller_sells);
+	}
+	
+	//Метод вывода продаж.
+	public int get_sells() {
+		return seller_sells;
+	}
+}
